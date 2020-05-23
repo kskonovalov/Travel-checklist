d92b672e8be6adf526e4eb3965666c66308aff2e
Konstantin <picprofit@gmail.com>
2020-05-23 08:24:05 +0300

    upd: npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
+import 'materialize-css';
+import {
+  TextInput,
+  Collection,
+  CollectionItem,
+  Button,
+  Icon,
+} from 'react-materialize';
 
-function App() {
+type Task = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+
+const App: React.FC = () => {
+  const tasks: Task[] = [
+    {
+      id: 1,
+      text: 'Test task',
+      done: false,
+    },
+    {
+      id: 2,
+      text: 'Other task',
+      done: true,
+    },
+  ];
+
   return (
     <div className="container">
-      <header className="App-header">
-        Hello
-      </header>
+      <TextInput id="TextInput-4" label="Добавить в чеклист" />
+      <Button node="button" waves="light">
+        Добавить
+        <Icon right>cloud</Icon>
+      </Button>
+      <Collection>
+        {tasks.map((item) => {
+          return (
+            <CollectionItem key={item.id} href="">
+              {item.text}
+            </CollectionItem>
+          );
+        })}
+      </Collection>
     </div>
   );
-}
+};
 
 export default App;
