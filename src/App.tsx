9fc90269b125d856290503ec584ab96c6702c0dd
Konstantin <picprofit@gmail.com>
2020-05-28 20:01:06 +0300

    fix: typo

--- src/App.tsx
+++ src/App.tsx
@@ -128,7 +128,7 @@ const App: React.FC = () => {
               label="Ссылка на Ваш личный чеклист путешественника"
               fullWidth={true}
               value={`${window.location.href}`}
-              helperText="Нажмите чтобы скопировать"
+              helperText="Нажмите, чтобы скопировать"
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
