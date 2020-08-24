a48130158baf20b4ec3e6d73d5f0275dc6d37e8f
Konstantin <picprofit@gmail.com>
2020-08-24 09:37:06 +0300

    add: actions types

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -8,56 +8,76 @@ import {
   EMPTY_STORE,
   ADD_TASK_TO_LIST
 } from './constants';
-import ITaskAction from './interfaces/ITaskAction';
+import taskInterface from '../interfaces/taskInterface';
+// import ITaskAction from './interfaces/ITaskAction';
 
-interface ITask {
-  task?: string;
-  id?: string;
-  completed?: boolean;
+interface ICommonAction {
+  type: string;
+  listId: string;
 }
+
+interface IListAction extends ICommonAction {
+  listTitle?: string;
+  tasks?: taskInterface[];
+}
+
+interface ITaskAction extends ICommonAction {
+  taskId?: string;
+  taskText?: string;
+}
+
+// interface ITask {
+//   task?: string;
+//   id?: string;
+//   completed?: boolean;
+// }
 
 // export const deleteTask = ({ id }: ITask): ITaskAction => ({
 //   type: DELETE_TASK,
 //   id
 // });
 
-export const setListId = ({ listId }: { listId: string }) => ({
+export const setListId = ({ listId }: { listId: string }): IListAction => ({
   type: SET_LIST_ID,
   listId
 });
 
-export const addList = ({ tasks, listId, listTitle }: any): any => ({
+export const addList = ({ tasks, listId, listTitle }: any): IListAction => ({
   type: ADD_LIST,
   listId,
   listTitle,
   tasks
 });
 
-export const toggleTaskInList = ({ listId, taskId }: any): any => ({
+export const toggleTaskInList = ({ listId, taskId }: any): ITaskAction => ({
   type: TOGGLE_TASK_IN_LIST,
   listId,
   taskId
 });
 
-export const deleteTaskInList = ({ listId, taskId }: any): any => ({
+export const deleteTaskInList = ({ listId, taskId }: any): ITaskAction => ({
   type: DELETE_TASK_IN_LIST,
   listId,
   taskId
 });
 
-export const editTaskInList = ({ listId, taskId, taskText }: any): any => ({
+export const editTaskInList = ({
+  listId,
+  taskId,
+  taskText
+}: any): ITaskAction => ({
   type: EDIT_TASK_IN_LIST,
   listId,
   taskId,
   taskText
 });
 
-export const addTaskToList = ({ listId, taskText }: any): any => ({
+export const addTaskToList = ({ listId, taskText }: any): ITaskAction => ({
   type: ADD_TASK_TO_LIST,
   listId,
   taskText
 });
 
-export const emptyStore = (): any => ({
+export const emptyStore = () => ({
   type: EMPTY_STORE
 });
