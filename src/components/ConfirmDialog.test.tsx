0bbd317896cfd116686cd9d7e3040d5473f4a0c2
Konstantin <picprofit@gmail.com>
2020-09-16 09:11:54 +0300

    add: buttons test

--- src/components/ConfirmDialog.test.tsx
+++ src/components/ConfirmDialog.test.tsx
@@ -7,12 +7,21 @@ configure({ adapter: new Adapter() });
 import ConfirmDialog from './ConfirmDialog';
 
 describe('Component: ConfirmDialog', () => {
-  test('should render component', () => {
+  test('should render component description', () => {
     // @ts-ignore
     const component = shallow(
       <ConfirmDialog open={true} onClose={() => {}} onConfirm={() => {}} />
     );
     const description = component.find('#alert-dialog-description');
     expect(description.length).toBe(1);
+  });
+  test('should render component buttons', () => {
+    // @ts-ignore
+    const component = shallow(
+      <ConfirmDialog open={true} onClose={() => {}} onConfirm={() => {}} />
+    );
+    const buttons = component.find('WithStyles(ForwardRef(Button))');
+    expect(buttons.length).toBe(2); // confirm & cancel
+    // console.log(component.debug());
   });
 });
